96f756de38121500f2ded06314f6b7f11107ecd1
Chitoru <80137671+Chitoru@users.noreply.github.com>
2024-11-14 14:20:38 +1100

    OWOWOOOW

--- js/week5.js
+++ js/week5.js
@@ -1,11 +1,14 @@
 console.log("Welcome to Week 5!");
 
 /* Exercise 1: Log the message "This week we meet the browser dev tools!" */
+console.log("This week we meet the browser dev tools!");
 
 /* Exercise 2: Set the main heading to be green and have font "Dancing Script" */
-
+let h1 = document.querySelector("h1");
+h1.style.color = "green";
+h1.style.fontFamily = "Dancing Script";
 /* Exercise 3: Set the main heading to be The Plant Shelf */
-
+h1.textContent = "The Plant Shelf";
 /* Exercise 4: Make the main heading toggle between the original style and the green/Dancing Script style when clicked */
 
 /* Exercise 5: Make the Logo spin around when clicked */
